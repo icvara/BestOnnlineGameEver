b5cfa3dde29be6fe71c099e59cc04e7b191486c6
Icvara <icvara.aor@gmail.com>
2025-09-28 21:13:04 +0100

    bud sa mere multi

--- world/main.gd
+++ world/main.gd
@@ -6,13 +6,16 @@ extends Node3D
 var id = 0
 
 func _ready() -> void:
-	print(id)
+	print(self.get_path()) 
+	print(multiplayer.get_unique_id())
 
 	#add_player(multiplayer.get_unique_id())
 	add_player.rpc_id(1,multiplayer.get_unique_id())
 
 @rpc("any_peer","call_local")
 func add_player(id):
+	print(self.get_path()) 
+	print(name)
 	print(id)
 	var new_player = player_scene.instantiate()
 	new_player.name = str(id)
