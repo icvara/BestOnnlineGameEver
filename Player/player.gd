cbb14b1a3504f6ac32575ee5d48948aeb0e1fb9d
Kyunsei <kirbyvaroujan@gmail.com>
2025-09-28 20:48:03 +0200

    Ball char need work on camera

--- Player/player.gd
+++ Player/player.gd
@@ -1,15 +1,28 @@
-extends CharacterBody3D
+extends RigidBody3D
 
 @export var speed = 100
-
-
-
-func _process(delta):
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = Vector3(input_direction.x,0,input_direction.y) * speed * delta
-
+@export var force_strength: float = 20.0
+@export var torque_strength: float = 5.0
 
 
 func _physics_process(delta: float) -> void:
+	var input_dir := Vector3.ZERO
 
-	move_and_slide()
+	# WASD / Arrow input
+	if Input.is_action_pressed("up"):
+		input_dir.z -= 1
+	if Input.is_action_pressed("down"):
+		input_dir.z += 1
+	if Input.is_action_pressed("left"):
+		input_dir.x -= 1
+	if Input.is_action_pressed("right"):
+		input_dir.x += 1
+	input_dir = input_dir.normalized()
+
+	if input_dir != Vector3.ZERO:
+		# Apply force to push the sphere
+		apply_central_force(input_dir * force_strength)
+
+	# Optional: make it roll more naturally by applying torque
+		var torque = Vector3(input_dir.z, 0, -input_dir.x) * torque_strength
+		apply_torque(torque)
